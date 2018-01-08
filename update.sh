7ccf748354370ac9347b70107f68e9c40f92aab1
kevin-home <936563422@qq.com>
2018-01-08 21:14:08 +0800

    更新

---
+++ update.sh
@@ -0,0 +1,10 @@
+﻿echo "project status and branch information:"
+echo "git status && git branch -a"
+git status && git branch -a
+
+echo "project file upload......."
+echo "git add. && git commit -m && git push "
+git add . && git commit -m "更新" && git push
+echo "upload finished!"
+
+read -n 1
